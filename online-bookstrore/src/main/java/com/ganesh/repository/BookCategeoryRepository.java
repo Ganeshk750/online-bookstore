136416334c17d328a7fdf84a6366a4dcd5b5946c
Ganeshk750 <ganesh9ot@gmail.com>
2020-05-19 11:25:40 +0530

    changes done in BookCategoryRepository for resource path name

--- online-bookstrore/src/main/java/com/ganesh/repository/BookCategeoryRepository.java
+++ online-bookstrore/src/main/java/com/ganesh/repository/BookCategeoryRepository.java
@@ -1,11 +1,11 @@
 package com.ganesh.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ganesh.model.BookCategory;
 
-
+@RepositoryRestResource(collectionResourceRel="bookCategory", path="book-category")
 public interface BookCategeoryRepository extends JpaRepository<BookCategory, Long>{
 
 }
